6944e96cc52b0b67c08e6ee8dbe686049c64a6e1
Andreas Neubauer <Andreas4231@gmx.at>
2023-10-07 08:08:01 +0000

    Everything with VS Code

--- ue4/02_pointer/pointers.c
+++ ue4/02_pointer/pointers.c
@@ -21,28 +21,42 @@ void test_malloc(void) {
 }
 
 void print_char_array(char s[]) {
-   UNUSED(s);
-   // TODO
+  for (int i = 0; s[i] != '\0'; i += 1) {
+    putchar(s[i]);
+  }
 }
 
 void print_char_pointer(char *s) {
-  UNUSED(s);
-  // TODO
+  // for (int i = 0; s[i] != '\0'; i += 1) {
+  //   putchar(s[i]);
+  // }
+  while (*s != '\0') {
+    putchar(*s);
+    s += 1;
+  }
 }
 
 void test_print(void) {
-  // TODO
+  char s[] = "Hello World\n";
+  print_char_array(s);
+  print_char_pointer(s);
 }
 
 int my_strncmp(char *a, char *b , int maxlen) {
-  UNUSED(a); UNUSED(b); UNUSED(maxlen);
-  // TODO
+  int i;
+  for (i = 0; i < maxlen && a[i] != '\0' && b[i] != '\0'; i += 1) {
+    if (a[i] != b[i]) {
+      return (int)a[i] - (int)b[i];
+    }
+  }
+  if (a[i] != '\0') return +(int)a[i];
+  if (b[i] != '\0') return -(int)b[i];
   return 0;
 }
 #define MYCMP(a, b) DEBUG(my_strncmp(a, b, MIN_ALEN(a, b)))
 
 void test_my_strncmp(void) {
-#if 0
+#if 1
   char a[] = "AAA";
   char aa[] = "AAAA";
   char b[] = "AAB";
@@ -64,21 +78,40 @@ void test_my_strncmp(void) {
 }
 
 void print_array_of_pointers(const char *a[], int len) {
-  UNUSED(a); UNUSED(len);
-  // TODO
+  for (int i = 0; i < len; i += 1) {
+    printf("%s ", a[i]);
+  }
+
+  printf("\n");
 }
 
 void test_array_of_pointers(void) {
-  // TODO
+  const char *a[] = {"Hello", "dynamic array"};
+  print_array_of_pointers(a, ALEN(a));
 }
 
 void test_pointer_of_pointer(void) {
-  // TODO
+  int x = 123;
+  int *p = &x;
+  int **pp = &p;
+
+  printf("x = %d\n", x);
+  printf("pointer (%p) to x = %d\n", (void*)p, *p);
+  printf("pointer (%p) to pointer (%p) to x = %d\n", (void*)pp, (void*)*pp, **pp);
 }
 
 
+int add (int a, int b) { return a + b; }
+int sub (int a, int b) { return a - b; }
+char invalid_int_op (char a, char b) { return a + b; }
+
+typedef int (*binary_int_op_t) (int, int);
+
 void test_function_pointer(void) {
-  // TODO
+  binary_int_op_t ops[] = { &add, &sub };
+  for (int i = 0; i < ALEN(ops); i += 1) {
+    printf("operation %d: restult = %d\n", i, ops[i](3, 5));
+  }
 }
 
 int main(void) {
